dce6d5685bc4958be26b1ad287aea625ef014ae3
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2012-03-08 13:36:48 +0000

    Bluepill templating info didn't get passed along
    Aftering starting to use the user supplied config,
    the information was lost before reaching the
    bluepill configuration file.

--- cookbooks/signpost/recipes/monitoring.rb
+++ cookbooks/signpost/recipes/monitoring.rb
@@ -1,10 +1,18 @@
 # --- Setup a signpost service ---
 conf_path = File.expand_path node["signpost"]["config_path"]
 conf = YAML::load(File.open(conf_path))["config"]
-node["iodine_password"] = conf["iodine_password"]
 
 template "#{node["bluepill"]["conf_dir"]}/signpost_server.pill" do
   source "signpost_server.pill.erb"
+  variables(
+    :install_dir => node["signpost"]["installation_dir"],
+    :pid_dir => node["signpost"]["pid_dir"],
+    :log_dir => node["signpost"]["log_dir"],
+    :password => conf["iodine_password"],
+    :ip_slash_24 => conf["ip_slash_24"],
+    :signpost_number => conf["signpost_number"],
+    :domain => conf["domain"]
+  )
 end
 
 bluepill_service "signpost_server" do
